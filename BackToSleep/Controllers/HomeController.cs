3c33ddfeacb39540d086c9a702b7f37ae9f60581
stevojed <jedi1014@comcast.net>
2019-03-11 09:41:35 -0400

    Intial Commit

---
+++ BackToSleep/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using BackToSleep.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace BackToSleep.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return RedirectToAction("GetLocation");
+        }
+
+
+        public ActionResult GetLocation()
+        {            
+            string city = "Warren";
+            string state = "Michigan";
+            ViewBag.Location = GetData(GPlacesDAL.GetLatitude($"{city}+{state}"), GPlacesDAL.GetLongitude($"{city}+{state}"));
+            return View();
+        }
+
+        public static string GetData(string lat, string lng)
+        {
+            return GPlacesDAL.GetPlace(lat, lng);
+        }
+
+    }
+}
---
+++ BackToSleep/Models/GPlacesDAL.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace BackToSleep.Models
+{
+    public class GPlacesDAL
+    {
+        public static string GetLatitude(string UserAddress)
+        {
+            string url = "https://maps.googleapis.com/maps/api/geocode/json";
+            string ApiKey = ConfigurationManager.AppSettings["GoogleAPI"];
+            HttpWebRequest request = WebRequest.CreateHttp($"{url}?address={UserAddress}&key={ApiKey}");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string data = rd.ReadToEnd();
+            rd.Close();
+            JObject JData = JObject.Parse(data);
+            var Lat = JData["results"][0]["geometry"]["location"]["lat"].ToString();
+            return Lat;
+        }
+
+        public static string GetLongitude(string UserAddress)
+        {
+            string url = "https://maps.googleapis.com/maps/api/geocode/json";
+            string ApiKey = ConfigurationManager.AppSettings["GoogleAPI"];
+            HttpWebRequest request = WebRequest.CreateHttp($"{url}?address={UserAddress}&key={ApiKey}");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string data = rd.ReadToEnd();
+            rd.Close();
+            JObject JData = JObject.Parse(data);
+            var LNG = JData["results"][0]["geometry"]["location"]["lng"].ToString();
+            return LNG;
+        }
+        public static string GetPlace(string lat, string lng)
+        {
+            string url = $"https://api.yelp.com/v3/businesses/search?term=cafe&latitude={lat}&longitude={lng}";
+
+            HttpWebRequest webRequest = WebRequest.CreateHttp(url);
+           
+            webRequest.Headers.Add("Authorization", $"Bearer {ConfigurationManager.AppSettings["YelpAPIKey"]}");
+
+            HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse();
+            var stream = new StreamReader(webResponse.GetResponseStream(), Encoding.UTF8);
+            var content = stream.ReadToEnd();
+            stream.Close();
+
+            var Json = JObject.Parse(content);
+
+            string business = Json["businesses"][0]["url"].ToString();
+
+            return business;
+        }
+    }
+}
