4b49bac90ee1c3c17a1cbdcca7ce50875df58ec4
stevojed <jedi1014@comcast.net>
2019-03-12 16:48:59 -0400

    Controller logic

--- BackToSleep/Controllers/HomeController.cs
+++ BackToSleep/Controllers/HomeController.cs
@@ -10,18 +10,74 @@ namespace BackToSleep.Controllers
 {
     public class HomeController : Controller
     {
+        private SleeperDbContext db = new SleeperDbContext();
+
         public ActionResult Index()
         {
             return View();
-        }        
+        }
+
+        public string SleepData(int hours)
+        {
+
+            List<SleepDB> sleep = db.SleepDBs.ToList();
+
+            if (hours <= 2)
+            {
+                string YelpKey = (from yelp in sleep
+                                  where yelp.SleepHours == 2
+                                  select yelp.YelpKey).First();
+
+                return YelpKey;
+            }
+            else if (hours > 2 && hours < 6)
+            {
+                string YelpKey = (from yelp in sleep
+                                  where yelp.SleepHours == 4
+                                  select yelp.YelpKey).First();
+
+                return YelpKey;
+            }
+            else if (hours >= 6 && hours < 8 )
+            {
+                string YelpKey = (from yelp in sleep
+                                  where yelp.SleepHours == 6
+                                  select yelp.YelpKey).First();
+
+                return YelpKey;
+            }
+            else if (hours >= 8 && hours < 10)
+            {
+                string YelpKey = (from yelp in sleep
+                                  where yelp.SleepHours == 8
+                                  select yelp.YelpKey).First();
+
+                return YelpKey;
+            }
+            else if (hours >= 10)
+            {
+                string YelpKey = (from yelp in sleep
+                                  where yelp.SleepHours == 8
+                                  select yelp.YelpKey).First();
+
+                return YelpKey;
+            }
+            else
+            {
+                return null;
+            }
+        }
 
         public ActionResult GetLocation(int ZipCode)
         {
+            int hours = 3;
+
+            string YelpKey = SleepData(hours);
 
             string lat = GPlacesDAL.GetLatitude(ZipCode);
             string lng = GPlacesDAL.GetLongitude(ZipCode);
 
-            ViewBag.Business = GPlacesDAL.GetBusiness(lat, lng);
+            ViewBag.Business = GPlacesDAL.GetBusiness(lat, lng, YelpKey);
 
             ViewBag.Names = GPlacesDAL.GetName(ViewBag.Business);
 
--- BackToSleep/Controllers/SleepDBsController.cs
+++ BackToSleep/Controllers/SleepDBsController.cs
@@ -10,107 +10,107 @@ using BackToSleep.Models;
 
 namespace BackToSleep.Controllers
 {
-    public class DBController : Controller
+    public class SleepDBsController : Controller
     {
-        private BackToSleepDbEntities db = new BackToSleepDbEntities();
+        private SleeperDbContext db = new SleeperDbContext();
 
-        // GET: DB
+        // GET: SleepDBs
         public ActionResult Index()
         {
-            return View(db.Researches.ToList());
+            return View(db.SleepDBs.ToList());
         }
 
-        // GET: DB/Details/5
+        // GET: SleepDBs/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Research research = db.Researches.Find(id);
-            if (research == null)
+            SleepDB sleepDB = db.SleepDBs.Find(id);
+            if (sleepDB == null)
             {
                 return HttpNotFound();
             }
-            return View(research);
+            return View(sleepDB);
         }
 
-        // GET: DB/Create
+        // GET: SleepDBs/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: DB/Create
+        // POST: SleepDBs/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,Yelp_Keyword,Description")] Research research)
+        public ActionResult Create([Bind(Include = "ID,YelpKey,Description,SleepHours")] SleepDB sleepDB)
         {
             if (ModelState.IsValid)
             {
-                db.Researches.Add(research);
+                db.SleepDBs.Add(sleepDB);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            return View(research);
+            return View(sleepDB);
         }
 
-        // GET: DB/Edit/5
+        // GET: SleepDBs/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Research research = db.Researches.Find(id);
-            if (research == null)
+            SleepDB sleepDB = db.SleepDBs.Find(id);
+            if (sleepDB == null)
             {
                 return HttpNotFound();
             }
-            return View(research);
+            return View(sleepDB);
         }
 
-        // POST: DB/Edit/5
+        // POST: SleepDBs/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Yelp_Keyword,Description")] Research research)
+        public ActionResult Edit([Bind(Include = "ID,YelpKey,Description,SleepHours")] SleepDB sleepDB)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(research).State = EntityState.Modified;
+                db.Entry(sleepDB).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(research);
+            return View(sleepDB);
         }
 
-        // GET: DB/Delete/5
+        // GET: SleepDBs/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Research research = db.Researches.Find(id);
-            if (research == null)
+            SleepDB sleepDB = db.SleepDBs.Find(id);
+            if (sleepDB == null)
             {
                 return HttpNotFound();
             }
-            return View(research);
+            return View(sleepDB);
         }
 
-        // POST: DB/Delete/5
+        // POST: SleepDBs/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            Research research = db.Researches.Find(id);
-            db.Researches.Remove(research);
+            SleepDB sleepDB = db.SleepDBs.Find(id);
+            db.SleepDBs.Remove(sleepDB);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
