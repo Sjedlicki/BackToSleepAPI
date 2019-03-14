a88374447836860e7fdeb49197f492a960b24d97
Justin <justinsortor@gmail.com>
2019-03-14 09:48:02 -0400

    Added UserSleepData DB & Controller

--- BackToSleep/Models/IdentityModels.cs
+++ BackToSleep/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace BackToSleep.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("IdentityConnectionString", throwIfV1Schema: false)
         {
         }
 
